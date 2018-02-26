ddefd66a66ae5a79acf5bb767df5fb994b4563a7
Jonathan Beliën <jbe@geo6.be>
2018-02-26 12:44:53 +0100

    Update snapshot.php

--- snapshot.php
+++ snapshot.php
@@ -26,101 +26,131 @@ foreach ($config['projects'] as $p) {
     echo count($p['instances']).' INSTANCE(S)'.PHP_EOL;
     echo count($p['volumes']).' VOLUME(S)'.PHP_EOL;
 
-    try {
-        if (isset($config['duration']) && !empty($config['duration'])) {
-            $time = new DateTime();
-            $time->sub(new DateInterval($config['duration']));
-            echo 'Delete snapshots older than '.$time->format('Y-m-d H:i:s').PHP_EOL;
-
-            if ($dryrun !== true) {
-                $log->debug('PROJECT: {project} - Delete snapshots older than '.$time->format('Y-m-d H:i:s'), [
-                    'project' => $p['id'],
-                ]);
-            }
-
-            $count = 0;
-
-            $list = $ovh->get('/cloud/project/'.$p['id'].'/snapshot');
-            foreach ($list as $snapshot) {
-                $snapshot_time = new DateTime($snapshot['creationDate']);
-                if ($snapshot_time < $time) {
-                    if ($dryrun !== true) {
-                        $url = '/cloud/project/'.$p['id'].'/snapshot/'.$snapshot['id'];
-                        $delete = $ovh->delete($url);
-
-                        $log->debug('PROJECT: {project} - Delete snapshot "'.$snapshot['name'].'" ('.$snapshot_time->format('Y-m-d H:i:s').')', [
-                            'project'  => $p['id'],
-                            'snapshot' => $snapshot,
-                            'delete'   => $delete,
-                            'url'      => $url,
-                        ]);
-                    }
-
-                    echo 'Delete snapshot "'.$snapshot['name'].'" ('.$snapshot_time->format('Y-m-d H:i:s').')'.PHP_EOL;
-
-                    $count++;
-                }
-            }
-
-            $list = $ovh->get('/cloud/project/'.$p['id'].'/volume/snapshot');
-            foreach ($list as $snapshot) {
-                $snapshot_time = new DateTime($snapshot['creationDate']);
-                if ($snapshot_time < $time) {
-                    if ($dryrun !== true) {
-                        $url = '/cloud/project/'.$p['id'].'/volume/snapshot/'.$snapshot['id'];
-                        $delete = $ovh->delete($url);
-
-                        $log->debug('PROJECT: {project} - Delete snapshot "'.$snapshot['name'].'" ('.$snapshot_time->format('Y-m-d H:i:s').')', [
-                            'project'  => $p['id'],
-                            'snapshot' => $snapshot,
-                            'delete'   => $delete,
-                            'url'      => $url,
-                        ]);
-                    }
-
-                    echo 'Delete snapshot "'.$snapshot['name'].'" ('.$snapshot_time->format('Y-m-d H:i:s').')'.PHP_EOL;
-
-                    $count++;
-                }
-            }
-
-            echo sprintf('%d deleted snapshot(s)', $count).PHP_EOL;
-        }
+    if (isset($config['duration']) && !empty($config['duration'])) {
+        $time = new DateTime();
+        $time->sub(new DateInterval($config['duration']));
+        echo 'Delete snapshots older than '.$time->format('Y-m-d H:i:s').PHP_EOL;
 
         if ($dryrun !== true) {
-            $log->debug('PROJECT: {project} - Create snapshot of '.count($p['instances']).' instance(s)', [
+            $log->debug('PROJECT: {project} - Delete snapshots older than '.$time->format('Y-m-d H:i:s'), [
                 'project' => $p['id'],
             ]);
         }
 
-        foreach ($p['instances'] as $instance) {
-            echo 'INSTANCE: '.$instance['name'].PHP_EOL;
+        $count = 0;
 
-            if ($dryrun !== true) {
+        $list = $ovh->get('/cloud/project/'.$p['id'].'/snapshot');
+        foreach ($list as $snapshot) {
+            $snapshot_time = new DateTime($snapshot['creationDate']);
+            if ($snapshot_time < $time) {
+                if ($dryrun !== true) {
+                    try {
+                        $url = '/cloud/project/'.$p['id'].'/snapshot/'.$snapshot['id'];
+                        $delete = $ovh->delete($url);
+
+                        $log->debug('PROJECT: {project} - Deleted snapshot "'.$snapshot['name'].'" ('.$snapshot_time->format('Y-m-d H:i:s').')', [
+                            'project'  => $p['id'],
+                            'snapshot' => $snapshot,
+                            'delete'   => $delete,
+                            'url'      => $url,
+                        ]);
+                    } catch (ServerException $exception) {
+                        $log->error($exception->getMessage(), [
+                            'project'   => $p['id'],
+                            'snapshot'  => $snapshot,
+                            'delete'    => $delete,
+                            'url'       => $url,
+                            'exception' => $exception,
+                        ]);
+                    }
+                }
+
+                echo 'Delete snapshot "'.$snapshot['name'].'" ('.$snapshot_time->format('Y-m-d H:i:s').')'.PHP_EOL;
+
+                $count++;
+            }
+        }
+
+        $list = $ovh->get('/cloud/project/'.$p['id'].'/volume/snapshot');
+        foreach ($list as $snapshot) {
+            $snapshot_time = new DateTime($snapshot['creationDate']);
+            if ($snapshot_time < $time) {
+                if ($dryrun !== true) {
+                    try {
+                        $url = '/cloud/project/'.$p['id'].'/volume/snapshot/'.$snapshot['id'];
+                        $delete = $ovh->delete($url);
+
+                        $log->debug('PROJECT: {project} - Deleted snapshot "'.$snapshot['name'].'" ('.$snapshot_time->format('Y-m-d H:i:s').')', [
+                            'project'  => $p['id'],
+                            'snapshot' => $snapshot,
+                            'delete'   => $delete,
+                            'url'      => $url,
+                        ]);
+                    } catch (ServerException $exception) {
+                        $log->error($exception->getMessage(), [
+                            'project'   => $p['id'],
+                            'snapshot'  => $snapshot,
+                            'delete'    => $delete,
+                            'url'       => $url,
+                            'exception' => $exception,
+                        ]);
+                    }
+                }
+
+                echo 'Delete snapshot "'.$snapshot['name'].'" ('.$snapshot_time->format('Y-m-d H:i:s').')'.PHP_EOL;
+
+                $count++;
+            }
+        }
+
+        echo sprintf('%d deleted snapshot(s)', $count).PHP_EOL;
+    }
+
+    if ($dryrun !== true) {
+        $log->debug('PROJECT: {project} - Created snapshot of '.count($p['instances']).' instance(s)', [
+            'project' => $p['id'],
+        ]);
+    }
+
+    foreach ($p['instances'] as $instance) {
+        echo 'INSTANCE: '.$instance['name'].PHP_EOL;
+
+        if ($dryrun !== true) {
+            try {
                 $url = '/cloud/project/'.$p['id'].'/instance/'.$instance['id'].'/snapshot';
                 $snapshot = $ovh->post($url, [
                     'snapshotName' => $instance['name'].' ('.date('Y-m-d H:i:s').')',
                 ]);
 
-                $log->debug('PROJECT: {project} - Create snapshot instance "'.$instance['name'].'"', [
+                $log->debug('PROJECT: {project} - Created snapshot instance "'.$instance['name'].'"', [
                     'project'  => $p['id'],
                     'instance' => $instance,
                     'snapshot' => $snapshot,
                     'url'      => $url,
                 ]);
+            } catch (ServerException $exception) {
+                $log->error($exception->getMessage(), [
+                    'project'   => $p['id'],
+                    'instance'  => $instance,
+                    'snapshot'  => $snapshot,
+                    'url'       => $url,
+                    'exception' => $exception,
+                ]);
             }
         }
+    }
+
+    if ($dryrun !== true) {
+        $log->debug('PROJECT: {project} - Create snapshot of '.count($p['volumes']).' volume(s)', [
+            'project' => $p['id'],
+        ]);
+    }
+
+    foreach ($p['volumes'] as $volume) {
+        echo 'VOLUME: '.$volume['name'].PHP_EOL;
 
         if ($dryrun !== true) {
-            $log->debug('PROJECT: {project} - Create snapshot of '.count($p['volumes']).' volume(s)', [
-                'project' => $p['id'],
-            ]);
-        }
-
-        foreach ($p['volumes'] as $volume) {
-            echo 'VOLUME: '.$volume['name'].PHP_EOL;
-
-            if ($dryrun !== true) {
+            try {
                 $url = '/cloud/project/'.$p['id'].'/volume/'.$volume['id'].'/snapshot';
                 $snapshot = $ovh->post($url, [
                     'name' => $volume['name'].' ('.date('Y-m-d H:i:s').')',
@@ -132,13 +162,16 @@ foreach ($config['projects'] as $p) {
                     'snapshot' => $snapshot,
                     'url'      => $url,
                 ]);
+            } catch (ServerException $exception) {
+                $log->error($exception->getMessage(), [
+                    'project'   => $p['id'],
+                    'instance'  => $instance,
+                    'snapshot'  => $snapshot,
+                    'url'       => $url,
+                    'exception' => $exception,
+                ]);
             }
         }
-    } catch (ServerException $exception) {
-        $log->error($exception->getMessage(), [
-            'project'   => $p['id'],
-            'exception' => $exception,
-        ]);
     }
 }
 
